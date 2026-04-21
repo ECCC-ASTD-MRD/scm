512b42ae20d4d5a708c3d235c4ea1409fa1654ee
Chantal Pic <chantal.pic@ec.gc.ca>
2026-04-21 09:35:08 -0400

    Better check for GEM and SCM databases

--- scripts/link-dbase.sh
+++ scripts/link-dbase.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# link to database at CMC
+# link to GEM and SCM databases at CMC
+GEM_DBASE=/fs/ssm/eccc/mrd/rpn/MIG/GEM/d/gem-data/gem-data_4.2.0/gem-data_4.2.0_all/share/data/dfiles
+SCM_DBASE=/fs/ssm/eccc/mrd/rpn/MIG/SCM/d/scm-data/scm-data_0.15.1_all
 
-DOMAIN=`hostname -d`
-
-if [[ "${DOMAIN}"  = cmc.ec.gc.ca ]]; then
-    GEM_DBASE=/fs/ssm/eccc/mrd/rpn/MIG/GEM/d/gem-data/gem-data_4.2.0/gem-data_4.2.0_all/share/data/dfiles
-    [ -e ${GEM_DBASE} ] && [ ! -e gem_dbase ] && ln -sf ${GEM_DBASE} gem_dbase
-    SCM_DBASE=/fs/ssm/eccc/mrd/rpn/MIG/SCM/d/scm-data/scm-data_0.15.1_all
-    [ -e ${SCM_DBASE} ] && [ ! -e scm_dbase ] && ln -sf ${SCM_DBASE} scm_dbase
-elif [[ -z "${DOMAIN}" || ${DOMAIN} = "science.gc.ca" ]]; then
-    GEM_DBASE=/fs/ssm/eccc/mrd/rpn/MIG/GEM/d/gem-data/gem-data_4.2.0/gem-data_4.2.0_all/share/data/dfiles
-    [ -e ${GEM_DBASE} ] && [ ! -e gem_dbase ] && ln -sf ${GEM_DBASE} gem_dbase
-    SCM_DBASE=/fs/ssm/eccc/mrd/rpn/MIG/SCM/d/scm-data/scm-data_0.15.1_all
-    [ -e ${SCM_DBASE} ] && [ ! -e scm_dbase ] && ln -sf ${SCM_DBASE} scm_dbase
-else
-    echo "hostname not found: don't know where database is."
+if [ ! -d ${GEM_DBASE} ] ; then
+    echo "${GEM_DBASE} not found: don't know where database is."
+    exit 1
 fi
+
+if [ ! -d ${SCM_DBASE} ] ; then
+    echo "${SCM_DBASE} not found: don't know where database is."
+    exit 1
+fi
+
+# remove possible existing link or file, and create symbolic link, 
+# or display an error message (for example if a directory named gem_dbase already exists)
+\rm -f gem_dbase && ln -s ${GEM_DBASE} gem_dbase || ( echo "gem_dbase cannot be removed" && exit 2)
+\rm -f scm_dbase && ln -s ${SCM_DBASE} scm_dbase || ( echo "scm_dbase cannot be removed" && exit 2)
+
+
